26a034efce6a4afc7142cbee4f2b287a6d5ff20f
boyanaaaa <125132910+boyanaaaa@users.noreply.github.com>
2026-08-27 13:40:42 +0300

    Added  REPL input handling, command registry, help and exit commands.

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,43 @@
+import * as readline from "readline";
+import { getCommands } from "./commands.js";
+
 export function cleanInput(input: string): string[] {
   return input.trim().toLowerCase().split(/\s+/);
+}
+
+export function startREPL() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
+
+  const commands = getCommands();
+
+  rl.prompt();
+
+  rl.on("line", (input) => {
+    const words = cleanInput(input);
+
+    if (words.length === 0) {
+      rl.prompt();
+      return;
+    }
+
+    const command = commands[words[0]];
+
+    if (!command) {
+      console.log("Unknown command");
+      rl.prompt();
+      return;
+    }
+
+    try {
+      command.callback(commands);
+    } catch (err) {
+      console.error(err);
+    }
+
+    rl.prompt();
+  });
 }
